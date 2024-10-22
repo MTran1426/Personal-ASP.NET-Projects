acd9e04fc39f72f83ea18c330e4e9cd59645d080
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-10-21 18:15:11 -0600

    Changed some text and added a Databases page.

---
+++ ProjectOne/ProjectOne/Pages/DataSheet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectOne.Pages
+{
+    public class DataSheetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
