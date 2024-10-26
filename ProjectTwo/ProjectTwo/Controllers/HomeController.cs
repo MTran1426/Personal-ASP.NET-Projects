33b798d65af0fd72d8743d72920e43953f4e596e
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-10-25 18:17:00 -0600

    Added a return to the DataSheet View with a blank page.

--- ProjectTwo/ProjectTwo/Controllers/HomeController.cs
+++ ProjectTwo/ProjectTwo/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ProjectTwo.Controllers
             return View();
         }
 
+        public IActionResult DataSheet()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
